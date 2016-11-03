810442ecae7668f319b2e4a6d71e0e11367ff63e
colobas <guilherme.pg.pires@gmail.com>
2016-11-03 00:41:27 +0000

    seems to be working

--- trial.py
+++ trial.py
@@ -29,12 +29,12 @@ for i in range(5):
 
 # Inicialização do vector de temperaturas iniciais dos cores
 Tini = np.random.uniform(low=70.0, high=100.0, size=10)
-B = np.random.uniform(low=0.01, high=0.1, size=10)
+B = np.random.uniform(low=0.07, high=0.1, size=10)
 
 # Constantes do problema
+TMax = 100
 PMax = 4 # potência em Watts
 FMax = 1 # freq em GHz
-FTarget = 0.8
 
 #----------------------------------------------------------------------------
 
@@ -43,66 +43,69 @@ FTarget = 0.8
 P = Variable(K, 10) # Vectores das potências de cada core
 T = Variable(K, 10) # Vectores das temperaturas de cada core
 F = Variable(K, 10) # Vectores das freqs de cada core
-
+FTarget = Variable(1)
 #----------------------------------------------------------------------------
 
 # Definição do problema
-objective = Maximize(sum_entries(sum_entries(F, axis=1)))
+objective = Maximize(FTarget)
 
 constraints = []
 
 constraints.append(T[0,range(10)] == Tini) # T inicial é fixa
 
 for k in range(K):
+    if k > 0:
         for i in range(10):
-                if k > 0:
-                    N_temp = T[k-1, i] 
-                    for j in range(10):
-                        N_temp += A[i][j]*(T[k-1, j] - T[k-1, i])
+            N_temp = T[k-1, i] 
+            for j in range(10):
+                N_temp += A[i][j]*(T[k-1, j] - T[k-1, i])
 
-                    constraints.append(T[k, i] == (N_temp + B[i]*P[k, i]))
+            constraints.append(T[k, i] == (N_temp + B[i]*P[k, i]))
 
-                constraints.append(P[k, i] >= (PMax * (square(F[k, i]) / square(FMax))))
-                constraints.append(P[k, i] <= PMax)
-                constraints.append(F[k, i] >= 0)
-                constraints.append(F[k, i] <= FMax)
+    constraints.append(P[k,:] >= (PMax * (square(F[k,:] / FMax))))
+    constraints.append(P[k,:] <= PMax)
+    constraints.append(F[k,:] >= 0)
+    constraints.append(F[k,:] <= FMax)
+    constraints.append(T[k,:] <= TMax)
+    constraints.append(sum_entries(F[k, :]) >= 10*FTarget)
 
-constraints.append(sum_entries(F, axis=1) >= 10*FTarget)
 
 #-----------------------------------------------------------------------------
 
 prob = Problem(objective, constraints)
 result = prob.solve()
-
-
+print(result)
 
 import random
 def randomcolor():
     r = lambda: random.randint(0,255)
     return '#%02X%02X%02X' % (r(),r(),r())
 
-fig1 = plt.figure()
-fig2 = plt.figure()
-fig3 = plt.figure()
 
-Tplot = fig1.add_subplot(111)
-Fplot = fig2.add_subplot(111)
-Pplot = fig3.add_subplot(111)
 
-t = range(K)
+
+plt.figure(1)
+
 c = [randomcolor() for i in range(10)]
 
+
+plt.subplot(311)
+plt.title('T(k)')
 for i in range(10):
     s = T[range(K), i].value
-    Tplot.plot(t, s, color=c[i])
+    plt.plot(s, color=c[i])
 
+plt.subplot(312)
+plt.title('F(k)')
 for i in range(10):
     s = F[range(K), i].value
-    Fplot.plot(t, s, color=c[i])
+    plt.plot(s, color=c[i])
 
+plt.subplot(313)
+plt.title('P(k)')
 for i in range(10):
     s = P[range(K), i].value
-    Pplot.plot(t, s, color=c[i])
+    plt.plot(s, color=c[i])
 
 
 plt.show()
